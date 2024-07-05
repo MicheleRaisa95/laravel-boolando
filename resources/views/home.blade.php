001295791b0671a998654846af67563545b67fef
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-07-05 16:59:32 +0200

    upd aggiunto layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boolando</title>
-    @vite('resources/js/app.js')
-</head>
-<body>
-    @include('partials.header')
-    <main>
-        SONO IL MAIN
-    </main>
-    @include('partials.footer')
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title')
+
+HOME
+
+@endsection
+
+@section('main-content')
+<main>
+    <h3>
+        main
+    </h3>
+</main>
+@endsection
