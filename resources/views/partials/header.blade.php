ef1dadc97cccf7e5f2410fa88978e9c299683e17
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-07-05 17:34:37 +0200

    upd creato header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,34 @@
-<header>
-    SONO HEADER
+<header class="mr_bg-header">
+    <div class="container">
+        <div class="row text-withe">
+            {{-- menu links --}}
+            <div class="col fw-bold">
+                <ul class="d-flex align-items-center mb-0 list-unstyled gap-3">
+                    <li>
+                        Donna
+                    </li>
+                    <li>
+                        Uomo
+                    </li>
+                    <li>
+                        Bambino
+                    </li>
+                </ul>
+                {{-- menu links --}}
+            </div>
+                {{-- logo --}}
+                <div class="col d-flex justify-content-center align items center">
+                    <img class="img-fluid h-50" src="{{ Vite::asset('resources/img/boolean-logo.png')}}" alt="logo">
+                </div>
+                {{-- /logo --}}
+
+                {{-- icone --}}
+                <div class="col d-flex justify-content-end align-items-center gap-2 fs-4">
+                <span>user</span>
+                <span>&hearts;</span>
+                <span>chart</span>
+                </div>
+                {{-- /icone --}}
+        </div>
+    </div>
 </header>
